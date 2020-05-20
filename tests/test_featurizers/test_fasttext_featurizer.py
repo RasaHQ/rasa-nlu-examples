73c505284e0e2daac05c0a7eae1131057db1c273
Vincent <vincentwarmerdam@gmail.com>
2020-05-20 15:49:30 +0200

    actions

---
+++ tests/test_featurizers/test_fasttext_featurizer.py
@@ -0,0 +1,36 @@
+import itertools as it
+import pathlib
+
+import pytest
+from rasa.nlu.tokenizers.whitespace_tokenizer import WhitespaceTokenizer
+
+from rasa_nlu_examples.featurizers.dense_featurizer.fasttext import FastTextFeaturizer
+from .featurizer_checks import dense_feature_checks
+
+test_folder = pathlib.Path(__file__).parent.parent.absolute()
+cache_dir = str(test_folder / "data")
+file_name = "custom_fasttext_model.bin"
+
+config = {
+    "cache_dir": cache_dir,
+    "file": file_name
+}
+
+tokenizer = WhitespaceTokenizer()
+featurizer = FastTextFeaturizer(component_config=config)
+
+combinations = it.product(
+    [f for f in dense_feature_checks],
+    [tokenizer],
+    [featurizer],
+    ["", "hello", "hello there"]
+)
+
+
+def test_model_loaded():
+    assert featurizer
+
+
+@pytest.mark.parametrize("test_fn,tok,feat,msg", combinations)
+def test_featurizer_checks(test_fn, tok, feat, msg):
+    test_fn(tok, feat, msg)
