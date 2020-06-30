1df44e70a964390d3d3aabe3df0fbd4f9c17d993
Vincent <vincentwarmerdam@gmail.com>
2020-06-30 10:08:54 +0200

    added-pre-commit

--- tests/test_featurizers/test_bpemb_featurizer.py
+++ tests/test_featurizers/test_bpemb_featurizer.py
@@ -1,32 +1,22 @@
-import itertools as it
-import pathlib
-
 import pytest
 from rasa.nlu.tokenizers.whitespace_tokenizer import WhitespaceTokenizer
 
 from rasa_nlu_examples.featurizers.dense import BytePairFeaturizer
-from .featurizer_checks import dense_feature_checks
+from .featurizer_checks import dense_standard_test_combinations
 
-test_folder = pathlib.Path(__file__).parent.parent.absolute()
-cache_dir = str(test_folder / "data")
-file_name = "custom_fasttext_model.bin"
 
 config = dict(lang="en", vs=1000, dim=25, vs_fallback=True)
 tokenizer = WhitespaceTokenizer()
 featurizer = BytePairFeaturizer(component_config=config)
-
-combinations = it.product(
-    [f for f in dense_feature_checks],
-    [tokenizer],
-    [featurizer],
-    ["", "hello", "hello there", "hello there again", "this is quite interesting", "dude", "foo", "bar", "buzz"]
-)
 
 
 def test_model_loaded():
     assert featurizer
 
 
-@pytest.mark.parametrize("test_fn,tok,feat,msg", combinations)
+@pytest.mark.parametrize(
+    "test_fn,tok,feat,msg",
+    dense_standard_test_combinations(tokenizer=tokenizer, featurizer=featurizer),
+)
 def test_featurizer_checks(test_fn, tok, feat, msg):
     test_fn(tok, feat, msg)
