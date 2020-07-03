917ca4dae2a23c37dff2685b18e872c7cf0aef0e
Vincent <vincentwarmerdam@gmail.com>
2020-07-03 12:49:07 +0200

    better-testing

--- Makefile
+++ Makefile
@@ -1,7 +1,17 @@
+install:
+	python -m pip install -e .
+	pre-commit install
+	python tests/prepare_everything.py
+
 test:
+	python tests/prepare_everything.py
 	pytest -n 2
 
-flake:
-	flake8
+style:
+	black --check rasa_nlu_examples tests
+	flake8 rasa_nlu_examples tests
 
-check: test
+types:
+	pytype --keep-going rasa_nlu_examples -j 16
+
+check: style types test
--- tests/prepare_everything.py
+++ tests/prepare_everything.py
@@ -1,10 +1,14 @@
 import fasttext
+import pathlib
 
 if __name__ == "__main__":
     # This script adds a small fasttext model that is used in testing.
     # Otherwise we might need to download 6Gb files just for unit tests.
-    model = fasttext.train_unsupervised(
-        "tests/data/fasttext-dummy-data.txt", model="cbow", dim=10
-    )
-    model.save_model("tests/data/custom_fasttext_model.bin")
-    print("local model saved for fasttext")
+    if not pathlib.Path("tests", "data", "fasttext-dummy-data.txt").exists():
+        model = fasttext.train_unsupervised(
+            "tests/data/fasttext-dummy-data.txt", model="cbow", dim=10
+        )
+        model.save_model("tests/data/custom_fasttext_model.bin")
+        print("local model saved for fasttext")
+    else:
+        print("found local fasttext model")
