859dbb47c30021043da8be01a787e0cb78fb8687
Vincent <vincentwarmerdam@gmail.com>
2020-09-08 12:46:03 +0200

    extra-stanza-tests

--- tests/test_tokenizers/test_stanza_tokenizer.py
+++ tests/test_tokenizers/test_stanza_tokenizer.py
@@ -1,6 +1,14 @@
 import pytest
-from rasa.nlu.training_data import Message
-from rasa.nlu.constants import TEXT, TOKENS_NAMES
+
+from rasa.nlu.training_data import Message, TrainingData
+from rasa.nlu.constants import TEXT, TOKENS_NAMES, SPARSE_FEATURE_NAMES
+from rasa.nlu.tokenizers.whitespace_tokenizer import WhitespaceTokenizer
+from rasa.nlu.featurizers.sparse_featurizer.count_vectors_featurizer import (
+    CountVectorsFeaturizer,
+)
+from rasa.nlu.featurizers.sparse_featurizer.lexical_syntactic_featurizer import (
+    LexicalSyntacticFeaturizer,
+)
 from rasa_nlu_examples.tokenizers.stanzatokenizer import StanzaTokenizer
 
 
@@ -57,3 +65,52 @@ def test_stanza_pos():
         "NOUN",
         "",
     ]
+
+
+def fetch_sparse_features(txt, tokenizer, featurizer):
+    message = Message("my advices include to give advice and giving many greetings")
+    tokenizer.process(message)
+    featurizer.train(TrainingData([message]))
+    featurizer.process(message)
+    return message.get(SPARSE_FEATURE_NAMES[TEXT]).toarray()
+
+
+def test_component_changes_features_cvf():
+    """If there are no features we need to add them"""
+    tok_whitespace = WhitespaceTokenizer()
+    tok_stanza = StanzaTokenizer(
+        component_config={"lang": "en", "cache_dir": "tests/data/stanza"}
+    )
+    txt = "i am running and giving many greetings"
+    feats_whitespace = fetch_sparse_features(
+        txt, tok_whitespace, CountVectorsFeaturizer()
+    )
+    feats_stanza = fetch_sparse_features(txt, tok_stanza, CountVectorsFeaturizer())
+
+    # Because the lemma is being used internally we expect less features
+    assert feats_stanza.shape[1] < feats_whitespace.shape[1]
+
+
+def test_component_changes_features_lex():
+    """If there are no features we need to add them"""
+    tok_whitespace = WhitespaceTokenizer()
+    tok_stanza = StanzaTokenizer(
+        component_config={"lang": "en", "cache_dir": "tests/data/stanza"}
+    )
+    txt = "i am running and giving many greetings"
+    feats = {
+        "features": [
+            ["low", "title", "upper", "pos"],
+            ["BOS", "EOS", "low", "upper", "title", "digit", "pos"],
+            ["low", "title", "upper", "pos"],
+        ]
+    }
+    feats_whitespace = fetch_sparse_features(
+        txt, tok_whitespace, LexicalSyntacticFeaturizer(feats)
+    )
+    feats_stanza = fetch_sparse_features(
+        txt, tok_stanza, LexicalSyntacticFeaturizer(feats)
+    )
+
+    # Because the part of speech tags are now used we'd expect stanza to have more features here.
+    assert feats_stanza.shape[1] > feats_whitespace.shape[1]
