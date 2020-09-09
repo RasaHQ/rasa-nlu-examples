ec1b6cb1caa41ac52ed4c95848b4502fdb6b12fb
Vincent <vincentwarmerdam@gmail.com>
2020-09-09 17:36:56 +0200

    internal-test-refactor

--- tests/prepare_everything.py
+++ tests/prepare_everything.py
@@ -5,11 +5,13 @@ import stanza
 if __name__ == "__main__":
     # This script adds a small fasttext model that is used in testing.
     # Otherwise we might need to download 6Gb files just for unit tests.
-    if not pathlib.Path("tests", "data", "custom_fasttext_model.bin").exists():
+    if not pathlib.Path(
+        "tests", "data", "fasttext", "custom_fasttext_model.bin"
+    ).exists():
         model = fasttext.train_unsupervised(
-            "tests/data/fasttext-dummy-data.txt", model="cbow", dim=10
+            "tests/data/fasttext/fasttext-dummy-data.txt", model="cbow", dim=10
         )
-        model.save_model("tests/data/custom_fasttext_model.bin")
+        model.save_model("tests/data/fasttext/custom_fasttext_model.bin")
         print("local model saved for fasttext")
     else:
         print("found local fasttext model")
--- tests/test_cli/test_commandline.py
+++ tests/test_cli/test_commandline.py
@@ -2,24 +2,25 @@ import subprocess
 import pytest
 
 
-yml_files = [
+english_yml_files = [
     "stanza-tokenizer-config.yml",
     "fasttext-config.yml",
     "printer-config.yml",
     "bytepair-config.yml",
     "gensim-config.yml",
-    "thai_tokenizer-config.yml",
 ]
 
+non_english_files = [("thai-tokenizer-config.yml", "tests/data/nlu/th/nlu-th.md")]
 
-@pytest.mark.parametrize("fp", yml_files)
-def test_run_test_command(fp):
+
+@pytest.mark.parametrize("fp", english_yml_files)
+def test_run_test_command_english(fp):
     cmd = [
         "rasa",
         "test",
         "nlu",
         "-u",
-        "tests/data/nlu.md",
+        "tests/data/nlu/en/nlu.md",
         "--config",
         f"tests/configs/{fp}",
         "--cross-validation",
@@ -32,16 +33,51 @@ def test_run_test_command(fp):
     assert status.returncode == 0
 
 
-@pytest.mark.parametrize("fp", yml_files)
-def test_run_train_command(fp):
+@pytest.mark.parametrize("fp", english_yml_files)
+def test_run_train_command_english(fp):
     cmd = [
         "rasa",
         "train",
         "nlu",
         "-u",
-        "tests/data/nlu.md",
+        "tests/data/nlu/en/nlu.md",
         "--config",
         f"tests/configs/{fp}",
+    ]
+    status = subprocess.run(cmd)
+    assert status.returncode == 0
+
+
+@pytest.mark.parametrize("yml,nlu", non_english_files)
+def test_run_test_command_non_english(yml, nlu):
+    cmd = [
+        "rasa",
+        "test",
+        "nlu",
+        "-u",
+        nlu,
+        "--config",
+        f"tests/configs/{yml}",
+        "--cross-validation",
+        "--runs",
+        "1",
+        "--folds",
+        "2",
+    ]
+    status = subprocess.run(cmd)
+    assert status.returncode == 0
+
+
+@pytest.mark.parametrize("yml,nlu", non_english_files)
+def test_run_train_command_non_english(yml, nlu):
+    cmd = [
+        "rasa",
+        "train",
+        "nlu",
+        "-u",
+        nlu,
+        "--config",
+        f"tests/configs/{yml}",
     ]
     status = subprocess.run(cmd)
     assert status.returncode == 0
