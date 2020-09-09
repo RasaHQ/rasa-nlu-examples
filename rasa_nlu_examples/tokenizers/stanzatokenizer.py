2d9251cb650c6be4f852eff2df96ff6823e6f1ce
Vincent <vincentwarmerdam@gmail.com>
2020-09-09 17:17:04 +0200

    feedback-tanja

--- rasa_nlu_examples/tokenizers/stanzatokenizer.py
+++ rasa_nlu_examples/tokenizers/stanzatokenizer.py
@@ -23,15 +23,12 @@ class StanzaTokenizer(Tokenizer):
     defaults = {
         # What language to use
         "lang": None,
-        # What package to use, details on this can be found here:
-        # https://stanfordnlp.github.io/stanza/download_models.html
-        "pkg": "package",
         # Where to load the model
         "cache_dir": None,
     }
 
-    # the following language should not be tokenized using the WhitespaceTokenizer
-    language_list = [
+    # the StanzaTokenizer only supports languages from this list
+    supported_language_list = [
         "af",
         "grc",
         "ar",
@@ -103,10 +100,12 @@ class StanzaTokenizer(Tokenizer):
 
         super().__init__(component_config)
         self.nlp = stanza.Pipeline(
-            lang=component_config["lang"],
-            dir=component_config["cache_dir"],
-            processors="tokenize,pos,lemma",
-            tokenize_no_ssplit=True,
+            lang=component_config["lang"],  # the language model from Stanza to user
+            dir=component_config[
+                "cache_dir"
+            ],  # the caching directory to load the model from
+            processors="tokenize,pos,lemma",  # info: https://stanfordnlp.github.io/stanza/pipeline.html#processors
+            tokenize_no_ssplit=True,  # disable sentence segmentation
         )
 
     def tokenize(self, message: Message, attribute: Text) -> List[Token]:
@@ -114,13 +113,15 @@ class StanzaTokenizer(Tokenizer):
 
         doc = self.nlp(text)
         stanza_tokens = reduce(lambda a, b: a + b, doc.sentences).tokens
+        # In the code below, if Stanza detects multi-word tokens then we should not
+        # fill in the lemma/pos information. Otherwise we're good.
         return [
             Token(
                 text=t.text,
                 start=t.start_char,
                 end=t.end_char,
-                lemma=t.words[0].lemma,
-                data={POS_TAG_KEY: t.words[0].pos},
+                lemma=t.words[0].lemma if len(t.words) == 1 else None,
+                data={POS_TAG_KEY: t.words[0].pos} if len(t.words) == 1 else None,
             )
             for t in stanza_tokens
         ]
