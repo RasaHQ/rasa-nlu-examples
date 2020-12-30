a7e27757b665f4d38465707dae48c97bfd0d0534
koaning <vincentwarmerdam@gmail.com>
2020-12-30 14:35:02 +0100

    added-tests

---
+++ tests/test_scikit/test_common.py
@@ -0,0 +1,13 @@
+from rasa_nlu_examples.scikit import nlu_folder_to_dataframe, nlu_path_to_dataframe
+
+
+def test_yaml_nlu_equal_folder():
+    df1 = nlu_folder_to_dataframe("tests/data/nlu/en", kind="md")
+    df2 = nlu_folder_to_dataframe("tests/data/nlu/en-yml")
+    assert df1.shape == df2.shape
+
+
+def test_yaml_nlu_equal_path():
+    df1 = nlu_path_to_dataframe("tests/data/nlu/en/nlu.md")
+    df2 = nlu_path_to_dataframe("tests/data/nlu/en-yml/nlu_converted.yml")
+    assert df1.shape == df2.shape
