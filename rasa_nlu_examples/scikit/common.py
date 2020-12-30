bb91f4e374884e07f4ce95d3a34d812a6763b048
koaning <vincentwarmerdam@gmail.com>
2020-12-30 14:23:50 +0100

    new-code

--- rasa_nlu_examples/scikit/common.py
+++ rasa_nlu_examples/scikit/common.py
@@ -3,17 +3,31 @@ import pathlib
 import pandas as pd
 
 
-def nlu_folder_to_dataframe(path):
-    """
-    Converts the data in an NLU folder into a pandas Dataframe
-
-    Arguments:
-        path: refers to the `nlu` folder
-    """
+def nlu_path_to_dataframe(path):
+    """Converts a single nlu file with intents into a dataframe."""
     from rasa.nlu.convert import convert_training_data
 
     data = []
-    for p in pathlib.Path(path).glob("*.md"):
+    p = pathlib.Path(path)
+    name = p.parts[-1]
+    name = name[: name.find(".")]
+    convert_training_data(str(p), f"{name}.json", output_format="json", language="en")
+    blob = json.loads(pathlib.Path(f"{name}.json").read_text())
+    for d in blob["rasa_nlu_data"]["common_examples"]:
+        data.append({"text": d["text"], "label": d["intent"]})
+    pathlib.Path(f"{name}.json").unlink()
+    return pd.DataFrame(data)
+
+
+def nlu_folder_to_dataframe(path, kind="yml"):
+    """Converts a folder of nlu files into a single dataframe."""
+    from rasa.nlu.convert import convert_training_data
+
+    data = []
+    glob = list(pathlib.Path(path).glob(f"*.{kind}"))
+    if len(glob) == 0:
+        raise ValueError(f"Did not find any .{kind} files at {path}.")
+    for p in glob:
         name = p.parts[-1]
         name = name[: name.find(".")]
         convert_training_data(
