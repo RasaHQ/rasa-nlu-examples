faf27a4362fbaa050e9d9df9b166dfe2f5a5f442
koaning <vincentwarmerdam@gmail.com>
2021-01-22 17:31:56 +0100

    readme

--- setup.py
+++ setup.py
@@ -2,13 +2,11 @@ from setuptools import setup, find_packages
 
 
 base_packages = [
-    "rasa>=1.10.0",
+    "rasa>=2.2.0",
     "fasttext>=0.9.2",
     "bpemb>=0.3.2",
     "gensim>=3.8.3",
-    "pythainlp>=2.2.3",
     "rich>=9.2.0",
-    "stanza>=1.1.1",
     "pandas>=1.0.5",
 ]
 
@@ -24,11 +22,23 @@ dev_packages = [
     "pymdown-extensions>=7.1",
 ]
 
+stanza_deps = [
+    "stanza>=1.1.1",
+]
+
+thai_deps = [
+    "pythainlp>=2.2.3",
+]
 
 setup(
     name="rasa_nlu_examples",
-    version="0.2.0",
+    version="0.2.1",
     packages=find_packages(exclude=["notebooks"]),
     install_requires=base_packages,
-    extras_require={"dev": dev_packages},
+    extras_require={
+        "dev": dev_packages,
+        "all": dev_packages + stanza_deps + thai_deps,
+        "thai": base_packages + thai_deps,
+        "stanza": base_packages + stanza_deps,
+    },
 )
