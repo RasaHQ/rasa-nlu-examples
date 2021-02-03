73de0a8d6f008023abc7dd7789b5e53d31ef59b3
koaning <vincentwarmerdam@gmail.com>
2021-02-03 11:31:57 +0100

    update-stanza-dir

--- tests/prepare_everything.py
+++ tests/prepare_everything.py
@@ -18,4 +18,4 @@ if __name__ == "__main__":
 
     # This part of the script will download a stanza model.
     # If there's a more lightweight way of doing this I'd love to hear it.
-    stanza.download("en", dir="tests/data/stanza")
+    stanza.download("en", model_dir="tests/data/stanza")
