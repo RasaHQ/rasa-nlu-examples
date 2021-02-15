6e894b2eef57c6cc4395f0da75c87cb33292463c
koaning <vincentwarmerdam@gmail.com>
2021-02-15 10:14:16 +0100

    print-remove

--- rasa_nlu_examples/scikit/__init__.py
+++ rasa_nlu_examples/scikit/__init__.py
@@ -1,4 +1,4 @@
 from .classifier import RasaClassifier
-from .common import nlu_path_to_dataframe
+from .common import nlu_path_to_dataframe, dataframe_to_nlu_file
 
-__all__ = ["RasaClassifier", "nlu_path_to_dataframe"]
+__all__ = ["RasaClassifier", "nlu_path_to_dataframe", "dataframe_to_nlu_file"]
--- rasa_nlu_examples/scikit/common.py
+++ rasa_nlu_examples/scikit/common.py
@@ -1,3 +1,4 @@
+import yaml
 import json
 import pathlib
 import pandas as pd
@@ -27,3 +28,54 @@ def nlu_path_to_dataframe(path):
         data.append({"text": d["text"], "label": d["intent"]})
     pathlib.Path(f"{name}.json").unlink()
     return pd.DataFrame(data)
+
+
+def dataframe_to_nlu_file(dataf, write_path, text_col="text", label_col="intent"):
+    """
+    Converts a single DataFrame file with intents into a intents file for Rasa.
+
+    Note that you cannot use this method to add entities.
+
+    Usage:
+
+    ```python
+    import pandas as pd
+    from rasa_nlu_examples.scikit import dataframe_to_nlu_file
+
+    df = pd.DataFrame([
+        {"text": "i really really like this", "intent": "positive"},
+        {"text": "i enjoy this", "intent": "positive"},
+        {"text": "this is not my thing", "intent": "negative"}
+    ])
+
+    dataframe_to_nlu_file(df, write_path="path/to/nlu.yml")
+    ```
+
+    This will yield a file with the following contents:
+
+    ```
+    version: 2.0
+    nlu:
+    - intent: negative
+      examples: |
+      - this is not my thing
+    - intent: positive
+      examples: |
+      - i really really like this
+      - i enjoy this
+    ```
+    """
+    from rasa.nlu.convert import convert_training_data
+
+    result = {"version": str(2.0), "nlu": []}
+    for idx, group in dataf.groupby(label_col):
+        intent = group[label_col].iloc[0]
+        result["nlu"].append(
+            {"intent": intent, "examples": [t for t in group[text_col]],}
+        )
+    dump = (
+        yaml.dump(result, sort_keys=False)
+        .replace("examples:", "examples: |")
+        .replace("  -", "   -")
+    )
+    return pathlib.Path(write_path).write_text(dump)
