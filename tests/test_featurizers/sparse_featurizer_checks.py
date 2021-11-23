88362eebd4c967e7bc602d22221017bd2504f7c4
Matthias Leimeister <m.leimeister@rasa.com>
2021-11-23 14:45:07 +0100

    Do not use NLU pipeline in unit tests. Add sparse standard checks.

---
+++ tests/test_featurizers/sparse_featurizer_checks.py
@@ -0,0 +1,71 @@
+from rasa.nlu.tokenizers.tokenizer import Tokenizer
+from rasa.shared.nlu.constants import TEXT
+from rasa.nlu.constants import TOKENS_NAMES
+from rasa.shared.nlu.training_data.message import Message
+import itertools as it
+
+
+def test_component_requires_tokenizer(tokenizer, featurizer, msg):
+    """The component should have a tokenizer in its required components."""
+    req_components = featurizer.__class__.required_components()
+    predicate = [c == Tokenizer for c in req_components]
+    assert any(predicate)
+
+
+def test_component_no_features_on_no_tokens(tokenizer, featurizer, msg):
+    """The component does not set any sparse features if there are no tokens."""
+    message = Message({TEXT: msg})
+    featurizer.process(message)
+    seq_vecs, sen_vecs = message.get_sparse_features(TEXT, [])
+    assert not seq_vecs
+    assert not sen_vecs
+
+
+def test_component_adds_features(tokenizer, featurizer, msg):
+    """If there are no features we need to add them"""
+    message = Message({TEXT: msg})
+    tokenizer.process(message)
+    tokens = message.get(TOKENS_NAMES[TEXT])
+
+    featurizer.process(message)
+    seq_vecs, sen_vecs = message.get_sparse_features(TEXT, [])
+    assert seq_vecs.features.shape[0] == len(tokens)
+
+
+def test_component_does_not_remove_features(tokenizer, featurizer, msg):
+    """If there are features we need to add not remove them"""
+    message = Message({TEXT: msg})
+    tokenizer.process(message)
+    featurizer.process(message)
+    seq_vecs1, sen_vecs1 = message.get_sparse_features(TEXT, [])
+
+    featurizer.process(message)
+    seq_vecs2, sen_vecs2 = message.get_sparse_features(TEXT, [])
+
+    assert (seq_vecs1.features.shape[1] * 2) == seq_vecs2.features.shape[1]
+    assert (sen_vecs1.features.shape[1] * 2) == sen_vecs2.features.shape[1]
+
+
+sparse_feature_checks = (
+    test_component_adds_features,
+    test_component_does_not_remove_features,
+    test_component_no_features_on_no_tokens,
+    test_component_requires_tokenizer,
+)
+
+
+def sparse_standard_test_combinations(
+    tokenizer, featurizer, messages=None, feature_checks=sparse_feature_checks
+):
+    if not messages:
+        messages = [
+            "hello",
+            "hello there",
+            "hello there again",
+            "this is quite interesting",
+            "dude",
+            "foo",
+            "bar",
+            "buzz",
+        ]
+    return it.product([f for f in feature_checks], [tokenizer], [featurizer], messages)
--- tests/test_featurizers/test_hashing_featurizer.py
+++ tests/test_featurizers/test_hashing_featurizer.py
@@ -1,29 +1,88 @@
 import pathlib
+import pytest
 import scipy.sparse
 
 from rasa.model_training import train_nlu
 from rasa.shared.nlu.training_data.message import Message
 from rasa_nlu_examples.scikit import load_interpreter
+from rasa_nlu_examples.featurizers.sparse.hashing_featurizer import HashingFeaturizer
+from rasa.nlu.tokenizers.whitespace_tokenizer import WhitespaceTokenizer
+
+from .sparse_featurizer_checks import sparse_standard_test_combinations
+
+component_config = dict(n_features=1024, norm=None)
 
 
-nlu_data = "tests/data/nlu/en/nlu.md"
-mod = train_nlu(
-    nlu_data=nlu_data,
-    config="tests/configs/hashing-featurizer-config.yml",
-    output="models",
+@pytest.mark.parametrize(
+    "test_fn,tok,feat,msg",
+    sparse_standard_test_combinations(
+        tokenizer=WhitespaceTokenizer(),
+        featurizer=HashingFeaturizer(component_config=component_config),
+    ),
 )
+def test_auto_featurizer_checks(test_fn, tok, feat, msg):
+    test_fn(tok, feat, msg)
 
 
-def test_features_are_sparse():
-    interpreter = load_interpreter(*pathlib.Path(mod).parts)
-    msg = Message({"text": "am I talking to a bot"})
-    for p in interpreter.interpreter.pipeline:
-        p.process(msg)
+@pytest.fixture
+def whitespace_tokenizer() -> WhitespaceTokenizer:
+    return WhitespaceTokenizer()
 
-    for feature in msg.get_sparse_features("text"):
+
+@pytest.fixture
+def hashing_featurizer() -> HashingFeaturizer:
+    return HashingFeaturizer(component_config=component_config)
+
+
+def test_features_are_sparse(
+    whitespace_tokenizer: WhitespaceTokenizer,
+    hashing_featurizer: HashingFeaturizer,
+):
+    message = Message.build("am I talking to a bot")
+
+    whitespace_tokenizer.process(message)
+    hashing_featurizer.process(message)
+
+    for feature in message.features:
         assert scipy.sparse.issparse(feature.features)
 
-    feat_tok, feat_sent = msg.get_sparse_features("text")
+
+def test_feature_shapes(
+    whitespace_tokenizer: WhitespaceTokenizer,
+    hashing_featurizer: HashingFeaturizer,
+):
+    message = Message.build("am I talking to a bot")
+
+    whitespace_tokenizer.process(message)
+    hashing_featurizer.process(message)
+
+    feat_tok, feat_sent = message.get_sparse_features("text")
     assert feat_tok.features.shape == (6, 1024)
     assert feat_sent.features.shape == (1, 1024)
     assert feat_tok.features.sum() == feat_sent.features.sum()
+
+
+def test_feature_overlap(
+    whitespace_tokenizer: WhitespaceTokenizer,
+    hashing_featurizer: HashingFeaturizer,
+):
+    message_1 = Message.build("am I talking to a bot")
+    whitespace_tokenizer.process(message_1)
+    hashing_featurizer.process(message_1)
+
+    message_2 = Message.build("sharing a subset of words")
+    whitespace_tokenizer.process(message_2)
+    hashing_featurizer.process(message_2)
+
+    message_3 = Message.build("completely different message")
+    whitespace_tokenizer.process(message_3)
+    hashing_featurizer.process(message_3)
+
+    _, feat_sent_1 = message_1.get_sparse_features("text")
+    _, feat_sent_2 = message_2.get_sparse_features("text")
+    _, feat_sent_3 = message_3.get_sparse_features("text")
+
+    # the first ans second message have overlapping vocabulary, so some column
+    # index should contain non-zero values in both
+    assert feat_sent_1.features.dot(feat_sent_2.features.T) > 0
+    assert feat_sent_1.features.dot(feat_sent_3.features.T) == 0
