d238caa331b1ce5cb343bddf50af2b6b88142981
Vincent <vincentwarmerdam@gmail.com>
2021-12-23 15:35:06 +0100

    deal with semantic map featurizers

--- rasa_nlu_examples/featurizers/sparse/__init__.py
+++ rasa_nlu_examples/featurizers/sparse/__init__.py
@@ -1,9 +1,7 @@
-from .semantic_map_featurizer import SemanticMapFeaturizer
 from .sparse_bpemb_featurizer import SparseBytePairFeaturizer
 from .hashing_featurizer import HashingFeaturizer
 
 __all__ = [
-    "SemanticMapFeaturizer",
     "SparseBytePairFeaturizer",
     "HashingFeaturizer",
 ]
